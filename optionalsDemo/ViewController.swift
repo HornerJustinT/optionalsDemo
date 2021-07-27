de5af613024f6274edf8f8e4d6b20c07909981b6
HornerJustinT <Justin.Horner@revature.net>
2021-07-27 00:08:41 -0500

    Initial Commit

---
+++ optionalsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  optionalsDemo
+//
+//  Created by Justin Horner on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // swift introduces optionals in Swift 4.
+
+}
+
